611e73a402021f005f823d1dd3c6e5f6b979345a
Russ Storms <rstorms90@gmail.com>
2020-03-26 12:54:22 -0600

    projects added for placeholders

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -16,12 +16,52 @@ const Projects = () => {
         className="padded-section"
       >
         <h1>Projects</h1>
-        <span>Project 1</span>
-        <span>Project 2</span>
-        <span>Project 3</span>
-        <span>Project 4</span>
-        <span>Project 5</span>
-        <span>Project 6</span>
+        <div
+          className="projects-container"
+        >
+          <div className="project">
+            <h4>Project Title</h4>
+            <h5>Project Tech</h5>
+            <img src="https://www.fillmurray.com/240/160" alt="project" />
+            <p>Github Link</p>
+            <p>Website Link</p>
+          </div>
+          <div className="project">
+            <h4>Project Title</h4>
+            <h5>Project Tech</h5>
+            <img src="https://www.fillmurray.com/240/160" alt="project" />
+            <p>Github Link</p>
+            <p>Website Link</p>
+          </div>
+          <div className="project">
+            <h4>Project Title</h4>
+            <h5>Project Tech</h5>
+            <img src="https://www.fillmurray.com/240/160" alt="project" />
+            <p>Github Link</p>
+            <p>Website Link</p>
+          </div>
+          <div className="project">
+            <h4>Project Title</h4>
+            <h5>Project Tech</h5>
+            <img src="https://www.fillmurray.com/240/160" alt="project" />
+            <p>Github Link</p>
+            <p>Website Link</p>
+          </div>
+          <div className="project">
+            <h4>Project Title</h4>
+            <h5>Project Tech</h5>
+            <img src="https://www.fillmurray.com/240/160" alt="project" />
+            <p>Github Link</p>
+            <p>Website Link</p>
+          </div>
+          <div className="project">
+            <h4>Project Title</h4>
+            <h5>Project Tech</h5>
+            <img src="https://www.fillmurray.com/240/160" alt="project" />
+            <p>Github Link</p>
+            <p>Website Link</p>
+          </div>
+        </div>
       </section>
       <div className="sun"></div>
     </div>
