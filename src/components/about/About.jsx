b00cd0f3ec7463ee99a94eadb46874ea887673a3
Russ Storms <rstorms90@gmail.com>
2020-03-28 11:37:18 -0600

    going to black bg

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,7 +10,29 @@ const About = () => {
       className="About padded-section"
     >
       <h1 className="section-title">About Me</h1>
-      <h4>Hobbies</h4>
+      <article className="about-info-container">
+        <img
+          className="profile-pic"
+          src="http://placeimg.com/240/240/animals"
+          alt="picture of me"
+        />
+        <p>
+          Hey! I'm Russ, and I found my passion for clean UI when I was a kid
+          making and editing skateboarding videos with my brother. A love of video
+          editing led to learning Adobe Photoshop, and eventually Javascript!
+        </p>
+        <p>
+          A big fan of MMA (love Jiu Jitsu), outdoor activities, creative writing,
+          and spending time with my girlfriend and our little pup!
+        </p>
+        <p>
+          I am a detailed oriented person, immersed in the frontend spectrum, and enjoy
+          working on ambitious projects with a great team. Remote work is preferrable!
+        </p>
+      </article>
+      <div>
+
+      </div>
     </section>
   )
 }
