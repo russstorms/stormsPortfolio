f3c0259381a0b9a2b11d9193f125b928888637f1
Russ Storms <rstorms90@gmail.com>
2020-03-29 12:30:01 -0600

    fixed navbar on scroll but it adds padding

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { scrollToSection } from '../../helpers/scrollToSection'
 
 import './Navbar.css'
 
 const Navbar = () => {
 
+  useEffect(() => {
+    const header = document.getElementById('navbar')
+    const sticky = header.offsetTop
+    const scrollCallback = window.addEventListener('scroll', () => {
+      if (window.pageYOffset > sticky) {
+        header.classList.add('sticky')
+      } else {
+        header.classList.remove('sticky')
+      }
+    })
+    return () => {
+      window.removeEventListener('scroll', scrollCallback)
+    }
+  }, [])
+
   return (
     <section
       className="Navbar"
+      id="navbar"
     >
       <div className="nav-neon-border" />
       <nav
