245b6f1902aa02c9093520463794342bcccfc577
Russ Storms <rstorms90@gmail.com>
2020-04-01 09:40:08 -0600

    added svg

--- src/components/about/AboutSide.jsx
+++ src/components/about/AboutSide.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 import { useSpring, animated } from 'react-spring'
 import './About.css'
 
-const AboutAside = () => {
-  const props = useSpring({
-    from: { opacity: 0, left: '0%', top: '0%', width: '0%', height: '0%', background: 'lightgreen' },
-    to: async next => {
-      while (1) {
-        await next({ left: '0%', top: '0%', width: '100%', height: '100%', background: 'steelblue', opacity: 1 })
-        await next({ height: '50%', background: 'skyblue' })
-      }
-    },
-  })
-  return <animated.div className="script-box" style={props} />
+const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
+const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
+const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`
+const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`
+const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
+const trans5 = (x, y) => `translate3d(${x / 10 + 30}px,${y / 10 - 30}px,0)`
+const trans6 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
+
+const AboutSide = () => {
+  const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
+  return (
+    <div className="about-side-container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
+      <animated.div className="card1" style={{ transform: props.xy.interpolate(trans1) }} />
+      <animated.div className="card2" style={{ transform: props.xy.interpolate(trans2) }} />
+      <animated.div className="card3" style={{ transform: props.xy.interpolate(trans3) }} />
+      <animated.div className="card4" style={{ transform: props.xy.interpolate(trans4) }} />
+      <animated.div className="card5" style={{ transform: props.xy.interpolate(trans5) }} />
+      <animated.div className="card6" style={{ transform: props.xy.interpolate(trans6) }} />
+    </div>
+  )
 }
 
-export default AboutAside
+export default AboutSide
 
