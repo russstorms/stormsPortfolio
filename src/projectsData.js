0cc56612d5560bd642e612cc2fc1d03078708d58
Russ Storms <rstorms90@gmail.com>
2020-04-01 09:08:35 -0600

    before mouse paralla

--- src/projectsData.js
+++ src/projectsData.js
@@ -21,15 +21,6 @@ export const projectsData = [
   },
   {
     id: 2,
-    title: 'Gabee Coffee',
-    subTitle: '',
-    imageSrc: 'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585717789/Portfolio%20ScreenShots/GabeeCoffee.png',
-    url: 'https://www.gabeecoffee.com/',
-    githubLink: '',
-    description: 'Website for an unmatched coffee shop.'
-  },
-  {
-    id: 3,
     title: 'RigPark',
     subTitle: '',
     imageSrc: 'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585718102/Portfolio%20ScreenShots/RigPark.png',
@@ -38,13 +29,22 @@ export const projectsData = [
     description: 'Truck parking, simplified.'
   },
   {
-    id: 4,
+    id: 3,
     title: 'Quanta Services',
     subTitle: '(and child companies)',
     imageSrc: 'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585718174/Portfolio%20ScreenShots/Quanta.png',
     url: 'https://www.quantaservices.com/',
     githubLink: '',
     description: 'With the largest craft-skilled workforce, a best-in-class safety program, and unparalleled project execution.'
+  },
+  {
+    id: 4,
+    title: 'Gabee Coffee',
+    subTitle: '',
+    imageSrc: 'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585753201/Portfolio%20ScreenShots/GabeeCoffee.png',
+    url: 'https://www.gabeecoffee.com/',
+    githubLink: '',
+    description: 'Website for an unmatched coffee shop.'
   },
   {
     id: 5,
