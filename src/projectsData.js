6d32ac9dc069bf2cff3c51663dd3c8bd02412ce8
Russ Storms <rstorms90@gmail.com>
2020-04-16 15:37:40 -0600

    added react colors

--- src/projectsData.js
+++ src/projectsData.js
@@ -65,7 +65,7 @@ export const projectsData = [
     title: 'React Colors',
     subTitle: '',
     imageSrc:
-      'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585850810/Portfolio%20ScreenShots/ReactColors.png',
+      'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1587072976/Portfolio%20ScreenShots/ReactColors.png',
     url: 'http://react-colors.surge.sh/',
     githubLink: 'https://github.com/rstorms90/React-Colors',
     description: 'React color palette creator.',
