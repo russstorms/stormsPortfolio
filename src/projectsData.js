7e59e0fe9955bc08b4facfc8c26278734b7eef38
Russ Storms <rstorms90@gmail.com>
2020-04-16 14:26:24 -0600

    removed rigpark

--- src/projectsData.js
+++ src/projectsData.js
@@ -5,64 +5,63 @@ export const projectsData = [
     id: 0,
     title: 'chizetteArt',
     subTitle: '',
-    imageSrc: "https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585703973/Portfolio%20ScreenShots/chizetteArt.png",
+    imageSrc:
+      'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585703973/Portfolio%20ScreenShots/chizetteArt.png',
     url: 'https://www.chizetteart.com',
     githubLink: 'https://www.github.com/rstorms90/chizetteart',
-    description: 'Website for an artist to display art and sell prints.'
+    description: 'Website for an artist to display art and sell prints.',
   },
   {
     id: 1,
     title: 'LifeSafer',
     subTitle: '',
-    imageSrc: "https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585717524/Portfolio%20ScreenShots/lifeSafer.png",
+    imageSrc:
+      'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585717524/Portfolio%20ScreenShots/lifeSafer.png',
     url: 'https://www.lifesafer.com',
     githubLink: '',
-    description: 'LifeSafer is a national ignition interlock company that serves 45 states.'
+    description:
+      'LifeSafer is a national ignition interlock company that serves 45 states.',
   },
   {
     id: 2,
-    title: 'RigPark',
-    subTitle: '',
-    imageSrc: 'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585718102/Portfolio%20ScreenShots/RigPark.png',
-    url: 'https://www.rigpark.com',
+    title: 'Quanta Services',
+    subTitle: '(and child companies)',
+    imageSrc:
+      'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585718174/Portfolio%20ScreenShots/Quanta.png',
+    url: 'https://www.quantaservices.com/',
     githubLink: '',
-    description: 'Truck parking, simplified.'
+    description:
+      'With the largest craft-skilled workforce, a best-in-class safety program, and unparalleled project execution.',
   },
   {
     id: 3,
-    title: 'Quanta Services',
-    subTitle: '(and child companies)',
-    imageSrc: 'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585718174/Portfolio%20ScreenShots/Quanta.png',
-    url: 'https://www.quantaservices.com/',
+    title: 'Team Rebus',
+    subTitle: '',
+    imageSrc:
+      'https://res.cloudinary.com/rstorms90/image/upload/v1585718285/Portfolio%20ScreenShots/teamRebus.png',
+    url: 'https://www.teamrebus.com',
     githubLink: '',
-    description: 'With the largest craft-skilled workforce, a best-in-class safety program, and unparalleled project execution.'
+    description:
+      'Team Rebus helps contact centers fuse best practice workforce and quality management solutions with the company’s core values and culture.',
   },
   {
     id: 4,
-    title: 'Team Rebus',
+    title: 'Gabee Coffee',
     subTitle: '',
-    imageSrc: 'https://res.cloudinary.com/rstorms90/image/upload/v1585718285/Portfolio%20ScreenShots/teamRebus.png',
-    url: 'https://www.teamrebus.com',
+    imageSrc:
+      'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585753201/Portfolio%20ScreenShots/GabeeCoffee.png',
+    url: 'https://www.gabeecoffee.com/',
     githubLink: '',
-    description: 'Team Rebus helps contact centers fuse best practice workforce and quality management solutions with the company’s core values and culture.'
+    description: 'Website for an unmatched coffee shop.',
   },
   {
     id: 5,
-    title: 'Gabee Coffee',
-    subTitle: '',
-    imageSrc: 'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585753201/Portfolio%20ScreenShots/GabeeCoffee.png',
-    url: 'https://www.gabeecoffee.com/',
-    githubLink: '',
-    description: 'Website for an unmatched coffee shop.'
-  },
-  {
-    id: 6,
     title: 'React Colors',
     subTitle: '',
-    imageSrc: 'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585850810/Portfolio%20ScreenShots/ReactColors.png',
+    imageSrc:
+      'https://res.cloudinary.com/rstorms90/image/upload/c_fill,h_500,w_250/v1585850810/Portfolio%20ScreenShots/ReactColors.png',
     url: 'http://react-colors.surge.sh/',
     githubLink: 'https://github.com/rstorms90/React-Colors',
-    description: 'React color palette creator.'
+    description: 'React color palette creator.',
   },
-]
-
+];
