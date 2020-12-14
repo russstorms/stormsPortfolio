9192615233c7ae45bc1c10bebed658aeaf476b44
Russ Storms <rstorms90@gmail.com>
2020-12-14 16:40:42 -0700

    Cleanup

--- src/components/about/SkillsList.jsx
+++ src/components/about/SkillsList.jsx
@@ -1,182 +1,59 @@
 import React, { useState } from 'react';
-import {
-  DiReact,
-  DiSass,
-  DiNodejs,
-  DiPhp,
-  DiPostgresql,
-  DiGit,
-  DiNpm,
-  DiHeroku,
-} from 'react-icons/di';
-import {
-  SiRedux,
-  SiGatsby,
-  SiNextDotJs,
-  SiHtml5,
-  SiCss3,
-  SiGraphql,
-  SiApollographql,
-  SiSocketDotIo,
-  SiNetlify,
-} from 'react-icons/si';
+import { specialities } from './specialties';
 
 import './SkillsList.css';
 
 const SkillsList = () => {
-  const [specialties, setSpecialties] = useState('Front');
+  const [selected, setSelected] = useState('front');
 
-  const switchSpecialties = (e) =>
-    setSpecialties(e.target.getAttribute('data-val'));
+  const handleClick = (e) => setSelected(e.target.value);
 
-  const frontEndSpecialties = [
-    {
-      id: 0,
-      techName: 'React',
-      icon: <DiReact />,
-    },
-    {
-      id: 1,
-      techName: 'Redux',
-      icon: <SiRedux />,
-    },
-    {
-      id: 2,
-      techName: 'Gatsby',
-      icon: <SiGatsby />,
-    },
-    {
-      id: 3,
-      techName: 'Next',
-      icon: <SiNextDotJs />,
-    },
-    {
-      id: 4,
-      techName: 'HTML',
-      icon: <SiHtml5 />,
-    },
-    {
-      id: 5,
-      techName: 'CSS',
-      icon: <SiCss3 />,
-    },
-    {
-      id: 6,
-      techName: 'Sass',
-      icon: <DiSass />,
-    },
-  ];
+  const front = 'front';
+  const back = 'back';
+  const storage = 'storage';
 
-  const backEndSpecialties = [
-    {
-      id: 0,
-      techName: 'Node',
-      icon: <DiNodejs />,
-    },
-    {
-      id: 1,
-      techName: 'PHP',
-      icon: <DiPhp />,
-    },
-    {
-      id: 2,
-      techName: 'SQL',
-      icon: <DiPostgresql />,
-    },
-    {
-      id: 3,
-      techName: 'GraphQL',
-      icon: <SiGraphql />,
-    },
-    {
-      id: 4,
-      techName: 'Apollo',
-      icon: <SiApollographql />,
-    },
-    {
-      id: 5,
-      techName: 'Socket.io',
-      icon: <SiSocketDotIo />,
-    },
-  ];
-
-  const storageAndDeploymentSpecialties = [
-    {
-      id: 0,
-      techName: 'Github',
-      icon: <DiGit />,
-    },
-    {
-      id: 1,
-      techName: 'Netlify',
-      icon: <SiNetlify />,
-    },
-    {
-      id: 2,
-      techName: 'NPM',
-      icon: <DiNpm />,
-    },
-    {
-      id: 3,
-      techName: 'Heroku',
-      icon: <DiHeroku />,
-    },
-  ];
-
-  const setSpecialtyArray = () => {
-    let specialtyArray;
-
-    if (specialties === 'Front') {
-      specialtyArray = frontEndSpecialties;
-    } else if (specialties === 'Back') {
-      specialtyArray = backEndSpecialties;
-    } else if (specialties === 'Storage') {
-      specialtyArray = storageAndDeploymentSpecialties;
-    }
-
-    return specialtyArray;
-  };
+  const data = specialities[selected];
 
   return (
     <div className="Skills-List">
       <h2 className="skills-title">Specialties</h2>
 
       <div className="skills-btn-container">
-        <button
-          onClick={(e) => switchSpecialties(e)}
-          data-val="Front"
-          className={specialties === 'Front' ? `active` : ''}
+        <Button
+          onClick={handleClick}
+          value={front}
+          selected={selected === front}
         >
           Front-End Technologies
-        </button>
+        </Button>
 
-        <button
-          onClick={(e) => switchSpecialties(e)}
-          data-val="Back"
-          className={specialties === 'Back' ? `active` : ''}
-        >
+        <Button onClick={handleClick} value={back} selected={selected === back}>
           Back-End Technologies
-        </button>
+        </Button>
 
-        <button
-          onClick={(e) => switchSpecialties(e)}
-          data-val="Storage"
-          className={specialties === 'Storage' ? `active` : ''}
+        <Button
+          onClick={handleClick}
+          value={storage}
+          selected={selected === storage}
         >
           Storage & Deployment
-        </button>
+        </Button>
       </div>
 
       <div className="skills-list">
-        {setSpecialtyArray().map((skill) => (
-          <div key={skill.id}>
-            <div className="skills-icon">{skill.icon}</div>
-            <h4 className="skills-name">{skill.techName}</h4>
+        {data.map(({ id, icon, techName }) => (
+          <div key={id}>
+            <div className="skills-icon">{icon}</div>
+            <h4 className="skills-name">{techName}</h4>
           </div>
         ))}
       </div>
     </div>
   );
+};
+
+const Button = ({ selected, ...props }) => {
+  return <button className={selected ? 'active' : ''} {...props} />;
 };
 
 export default SkillsList;
---
+++ src/components/about/specialties.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import {
+  DiReact,
+  DiSass,
+  DiNodejs,
+  DiPhp,
+  DiPostgresql,
+  DiGit,
+  DiNpm,
+  DiHeroku,
+} from 'react-icons/di';
+import {
+  SiRedux,
+  SiGatsby,
+  SiNextDotJs,
+  SiHtml5,
+  SiCss3,
+  SiGraphql,
+  SiApollographql,
+  SiSocketDotIo,
+  SiNetlify,
+} from 'react-icons/si';
+
+export const specialities = {
+  front: [
+    {
+      id: 0,
+      techName: 'React',
+      icon: <DiReact />,
+    },
+    {
+      id: 1,
+      techName: 'Redux',
+      icon: <SiRedux />,
+    },
+    {
+      id: 2,
+      techName: 'Gatsby',
+      icon: <SiGatsby />,
+    },
+    {
+      id: 3,
+      techName: 'Next',
+      icon: <SiNextDotJs />,
+    },
+    {
+      id: 4,
+      techName: 'HTML',
+      icon: <SiHtml5 />,
+    },
+    {
+      id: 5,
+      techName: 'CSS',
+      icon: <SiCss3 />,
+    },
+    {
+      id: 6,
+      techName: 'Sass',
+      icon: <DiSass />,
+    },
+  ],
+  back: [
+    {
+      id: 0,
+      techName: 'Node',
+      icon: <DiNodejs />,
+    },
+    {
+      id: 1,
+      techName: 'PHP',
+      icon: <DiPhp />,
+    },
+    {
+      id: 2,
+      techName: 'SQL',
+      icon: <DiPostgresql />,
+    },
+    {
+      id: 3,
+      techName: 'GraphQL',
+      icon: <SiGraphql />,
+    },
+    {
+      id: 4,
+      techName: 'Apollo',
+      icon: <SiApollographql />,
+    },
+    {
+      id: 5,
+      techName: 'Socket.io',
+      icon: <SiSocketDotIo />,
+    },
+  ],
+  storage: [
+    {
+      id: 0,
+      techName: 'Github',
+      icon: <DiGit />,
+    },
+    {
+      id: 1,
+      techName: 'Netlify',
+      icon: <SiNetlify />,
+    },
+    {
+      id: 2,
+      techName: 'NPM',
+      icon: <DiNpm />,
+    },
+    {
+      id: 3,
+      techName: 'Heroku',
+      icon: <DiHeroku />,
+    },
+  ],
+};
