cb5430e808f21e1699136b4ca91292598f8b285c
Russ Storms <rstorms90@gmail.com>
2020-12-13 23:26:17 -0700

    Adding icons to skills

--- src/components/about/SkillsList.jsx
+++ src/components/about/SkillsList.jsx
@@ -1,41 +1,81 @@
 import React from 'react';
+import { DiReact } from 'react-icons/di';
+import { DiSass } from 'react-icons/di';
+import {
+  SiRedux,
+  SiGatsby,
+  SiNextDotJs,
+  SiHtml5,
+  SiCss3,
+  SiSass,
+} from 'react-icons/si';
 
 import './SkillsList.css';
 
 const SkillsList = () => {
   const frontEndSpecialties = [
-    'React',
-    'React Native',
-    'Redux',
-    'Gatsby',
-    'Next',
-    'HTML',
-    'CSS',
-    'Sass',
+    {
+      id: 0,
+      techName: 'React',
+      icon: <DiReact />,
+    },
+    {
+      id: 1,
+      techName: 'Redux',
+      icon: <SiRedux />,
+    },
+    {
+      id: 2,
+      techName: 'Gatsby',
+      icon: <SiGatsby />,
+    },
+    {
+      id: 3,
+      techName: 'Next',
+      icon: <SiNextDotJs />,
+    },
+    {
+      id: 4,
+      techName: 'HTML',
+      icon: <SiHtml5 />,
+    },
+    {
+      id: 5,
+      techName: 'CSS',
+      icon: <SiCss3 />,
+    },
+    {
+      id: 6,
+      techName: 'Sass',
+      icon: <DiSass />,
+    },
   ];
 
-  const backEndSpecialties = [
-    'Express.js',
-    'PHP',
-    'SQL',
-    'GraphQL',
-    'Apollo',
-    'Socket.io',
-  ];
+  // const backEndSpecialties = [
+  //   'Express.js',
+  //   'PHP',
+  //   'SQL',
+  //   'GraphQL',
+  //   'Apollo',
+  //   'Socket.io',
+  // ];
 
-  const storageAndDeploymentSpecialties = [
-    'Github',
-    'Netlify',
-    'NPM',
-    'Heroku',
-  ];
+  // const storageAndDeploymentSpecialties = [
+  //   'Github',
+  //   'Netlify',
+  //   'NPM',
+  //   'Heroku',
+  // ];
 
   return (
     <div className="Skills-List-Container">
       <h2 className="skills-title">Specialties</h2>
       <div className="skills-list">
         {frontEndSpecialties.map((skill, idx) => (
-          <div key={idx}>{skill}</div>
+          <div key={idx}>
+            <div>{skill.icon}</div>
+            <h4>{skill.techName}</h4>
+          </div>
         ))}
       </div>
       {/* <div className="skills-list">
